96e98bddc6ac1e716f1f9c2892f3367e53b2edc7
thomas <minarovic.thomas@gmail.com>
2025-10-05 14:50:39 +0200

    add test controller

---
+++ Controllers/AzureTestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessRestApi.Controllers
+{
+    [Route("azure")]
+    [ApiController]
+    public class AzureTestController : ControllerBase
+    {
+        [HttpGet("test")]
+        public ActionResult<string> Get()
+        {
+            return Ok("Azure test endpoint is working!");
+        }
+    }
+}
